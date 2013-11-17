e4a17f91a8f539bd1126c96547c0b8163bc245c2
adamjacobbecker <ad@mbecker.cc>
2013-11-16 21:13:31 -0800

    mas specs

--- spec/lib/filterer/base_spec.rb
+++ spec/lib/filterer/base_spec.rb
@@ -26,11 +26,27 @@ describe Filterer::Base do
 
   it 'basic smoke test' do
     FakeQuery.any_instance.should_receive(:limit).with(10).and_return(FakeQuery.new)
+    FakeQuery.any_instance.should_receive(:offset).with(0).and_return(FakeQuery.new)
 
     @filterer = SmokeTestFilterer.new
     @filterer.meta[:page].should == 1
     @filterer.meta[:last_page].should == 1
     @filterer.meta[:total].should == FakeQuery::COUNT
+  end
+
+  it 'passes parameters to the correct methods' do
+    SmokeTestFilterer.any_instance.should_receive(:param_foo).with('bar').and_return(FakeQuery.new)
+    @filterer = SmokeTestFilterer.new({ foo: 'bar' })
+  end
+
+  it 'does not pass the :page parameter' do
+    SmokeTestFilterer.any_instance.should_not_receive(:param_page)
+    @filterer = SmokeTestFilterer.new({ page: 'bar' })
+  end
+
+  it 'does not pass blank parameters' do
+    SmokeTestFilterer.any_instance.should_not_receive(:param_foo)
+    @filterer = SmokeTestFilterer.new({ foo: '' })
   end
 
 end
